8d3dee5af6779db48d08abe364ee8ebef96f05a7
Youssef TAIRI <tairi.youssef@gmail.com>
2023-03-20 17:12:03 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - this script prints 0 to 9 using putchar
+* Return: 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		putchar(x + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
