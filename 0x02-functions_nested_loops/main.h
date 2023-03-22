1f8b86b0e6c729b0e054233fabe89e8709436ce9
Youssef TAIRI <tairi.youssef@gmail.com>
2023-03-22 12:46:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main 
-#define main
+#ifndef main_prototype
+#define main prototype
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -14,4 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif /* main */
+#endif
